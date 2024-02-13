afd75c6a5dd6edd71e4b4a7953970f268e352b28
loudovsky <luileptitecossais@gmail.com>
2024-02-13 14:24:24 +0100

    afternoon commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,32 @@
 const left = document.querySelector('.left')
+const right = document.querySelector('.right')
 
 fetch('./datas.json')
   .then(response => response.json())
   .then(data => {
         console.log(data);
-        left.innerHTML= "";
-        data.users.forEach(function(element){
-            left.innerHTML += `<div class="image"><img src="${element.image}" alt="gif de ouf"></div>`
+        data.users.forEach(function(element, index){
+            left.innerHTML += `<img src="${element.image}" alt="img de célib" class="image" data-index="${index}">`
         })
       })
       .catch(error => {console.log("Erreur lors de la récup des données :", error);
     })
+
+function generate(index) {
+  fetch('./datas.json')
+  .then(response => response.json())
+  .then(data => {
+        console.log(data);
+        right.innerHTML = `<img src="${data.users[index].image}" alt="img de célib"><h3>${data.users[index].name}</h3><h4>${data.users[index].age} ans</h4><a href="mailto:${data.users[index].email}">💌</a><p>${data.users[index].address.street} - ${data.users[index].address.city} (${data.users[index].address.country})</p>`
+      })
+      .catch(error => {console.log("Erreur lors de la récup des données :", error);
+    })
+}
+
+left.addEventListener('click', function(e) {
+    if (e.target.classList.contains('image')) {
+      let position = parseInt(e.target.getAttribute('data-index'));
+      console.log(position);
+      generate(position);
+    }
+  });
